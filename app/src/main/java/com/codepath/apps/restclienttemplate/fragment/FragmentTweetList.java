2f563e90df093e96b1b1e4878ba2c24cb53db5a3
Sanal Kumar <sanat@cisco.com>
2017-10-07 00:26:27 -0700

    Fix home timeline

--- app/src/main/java/com/codepath/apps/restclienttemplate/fragment/FragmentTweetList.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/fragment/FragmentTweetList.java
@@ -39,10 +39,10 @@ public class FragmentTweetList extends Fragment {
     private TweetAdapter adapter;
     ArrayList<Tweet> tweets;
     RecyclerView rvTweets;
-    private Long lastTweetId = 0L;
     SwipeRefreshLayout swipeRefreshLayout;
     EndlessRecyclerViewScrollListener scrollListener;
     ArrayList<Tweet> renderTweets;
+    LinearLayoutManager linearLayoutManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
@@ -52,32 +52,19 @@ public class FragmentTweetList extends Fragment {
         adapter = new TweetAdapter(tweets);
         renderTweets = new ArrayList<Tweet>();
 
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager = new LinearLayoutManager(getContext());
         rvTweets.setLayoutManager(linearLayoutManager);
         rvTweets.setAdapter(adapter);
-        // Retain an instance so that you can call `resetState()` for fresh searches
-        scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
-            @Override
-            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
-                // Triggered only when new data needs to be appended to the list
-                // Add whatever code is needed to append new items to the bottom of the list
-                fetchTimelineAsync();
-            }
-        };
-        rvTweets.addOnScrollListener(scrollListener);
+
         swipeRefreshLayout = (SwipeRefreshLayout) v.findViewById(R.id.swipeContainer);
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                lastTweetId = 0L;
-                fetchTimelineAsync();
-            }
-        });
+
         return v;
     }
 
-    public void addItems(JSONArray response) {
+    public Long addItems(JSONArray response) {
         adapter.clear();
+        Long lastTweetId = 0L;
+
         //iterate through json array
         for (int i = 0; i < response.length(); i++) {
             //for each entry deserialise the item
@@ -102,35 +89,6 @@ public class FragmentTweetList extends Fragment {
         Log.d("debug", "tweet size" + tweets.size());
         //notify adapter
         adapter.notifyItemInserted(tweets.size());
-    }
-
-
-    public void fetchTimelineAsync() {
-        TwitterClient client = TwitterApp.getRestClient();
-        if (isNetworkAvailable()) {
-            client.getHomeTimeline(new JsonHttpResponseHandler() {
-                @Override
-                public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                    Log.d("debug", response.toString());
-                    addItems(response);
-                    swipeRefreshLayout.setRefreshing(false);
-                }
-
-                @Override
-                public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
-                    super.onFailure(statusCode, headers, throwable, errorResponse);
-                }
-            },lastTweetId);
-        } else {
-            List<Tweet> tweetList = SQLite.select().from(Tweet.class).queryList();
-            Collections.reverse(tweetList);
-            tweets.addAll(tweetList);
-        }
-    }
-    private boolean isNetworkAvailable() {
-        ConnectivityManager connectivityManager
-                = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+        return lastTweetId;
     }
 }
--- app/src/main/java/com/codepath/apps/restclienttemplate/fragment/HomeTimelineFragment.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/fragment/HomeTimelineFragment.java
@@ -6,12 +6,15 @@ import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 
 import com.codepath.apps.restclienttemplate.R;
 import com.codepath.apps.restclienttemplate.TwitterApp;
 import com.codepath.apps.restclienttemplate.client.TwitterClient;
+import com.codepath.apps.restclienttemplate.listener.EndlessRecyclerViewScrollListener;
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.raizlabs.android.dbflow.sql.language.SQLite;
@@ -28,16 +31,59 @@ import cz.msebera.android.httpclient.Header;
  */
 
 public class HomeTimelineFragment extends FragmentTweetList {
+    private Long lastTweetId = 0L;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         fetchTimelineAsync();
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                fetchTimelineAsync();
+            }
+        };
+        rvTweets.addOnScrollListener(scrollListener);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                lastTweetId = 0L;
+                fetchTimelineAsync();
+            }
+        });
+    }
 
+    public void fetchTimelineAsync() {
+        TwitterClient client = TwitterApp.getRestClient();
+        if (isNetworkAvailable()) {
+            client.getHomeTimeline(new JsonHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                    Log.d("debug", response.toString());
+                    lastTweetId = addItems(response);
+                    swipeRefreshLayout.setRefreshing(false);
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+                    super.onFailure(statusCode, headers, throwable, errorResponse);
+                }
+            },lastTweetId);
+        } else {
+            List<Tweet> tweetList = SQLite.select().from(Tweet.class).queryList();
+            Collections.reverse(tweetList);
+            tweets.addAll(tweetList);
+        }
+    }
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
